225edb7afca4d604a68fcdd601b7b5c9eb9e79e7
jomolharii <anna_wozniak@posteo.net>
2023-05-25 10:57:01 +0200

    neu

---
+++ M_5_01.py
@@ -0,0 +1,23 @@
+from  PyQt6.QtWidgets import (QHBoxLayout, QLabel, QMainWindow, QGridLayout, QWidget, QFormLayout, QLineEdit, QApplication, QPushButton)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.setWindowTitle("GUI")
+        self.container = QWidget()
+        self.main_layout = QHBoxLayout()
+        
+        self.button = QPushButton("Click!")
+        self.button1 = QPushButton("Reset")
+        self.label = QLabel("Content")
+
+        self.main_layout.addwidget(self.button)
+        self.main_layout.addwidget(self.label)
+        self.main_layout.setLayout(self.main_layout)
+     
+                         
+app = QApplication([])
+window = MainWindow()
+window.show()
+app.exec()  
